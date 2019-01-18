fee04203cfd35aa90d4a11b258d90e69af966ee9
unknown <ialex.roman@icloud.com>
2019-01-18 12:19:09 -0500

    added menu chea we out here kid and we learning

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,35 @@
-
-
+let openBtn = document.getElementById('mySidenav');
+let sidebar = document.querySelector('.sidebar');
+let closeBtn = document.getElementById('closeBtn');
+let classNav = document.querySelector('.openNav');
+let idSideBar = document.getElementById('sidebar');
 // kfsadlkfjdsladkfj
 function openNav() {
-    let openBtn = document.getElementById('mySidenav');
+    
+    // btn click to make nav slide and open up
     openBtn.addEventListener('click', function() {
-        document.querySelector('.sidebar').style.width = "400px";
-        openBtn.classList = "openNav"
+       // document.querySelector('.sidebar').style.width = "400px";
+        sidebar.classList = "openNav";
+        //document.querySelector('.openNav').style.width = "22%";
 
-    })
+        // remove open btn
+        openBtn.style.display = 'none';
+
+        // display close btn
+        closeBtn.style.display = 'block';
+
+
+    });
 }
 
 //close da damn thing
 function closeNav() {
-    let closeBtn = document.getElementById('closeBtn');
     closeBtn.addEventListener('click', function() {
-        closeBtn.style.width = '0';
-    })
+        idSideBar.classList = 'sidebar';
+
+        openBtn.style.display = 'block';
+
+        closeBtn.style.display = 'none';
+    });
 }
+   
